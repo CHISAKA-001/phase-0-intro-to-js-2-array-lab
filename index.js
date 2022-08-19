3680b8a9bbdeb7d0b17a84aad87f6f577f9df107
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-19 12:54:13 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 // Write your solution here!
+//is assigned an initial value of ["Milo", "Otis", "Garfield"]
+const cats = ["Milo","Otis","Garfield"]
 
-
-const cats= ["Milo", "Otis", "Garfield"];
-// appends a cat to the end of the cats array
-
+//appends a cat to the end of the cats array
 function destructivelyAppendCat(name){
-cats.push(name);
-return cats;
+    cats.push(name);
 }
-//  prepends a cat to the beginning of the cats array
 
+//destructivelyPrependCat(name)
 function destructivelyPrependCat(name){
-    return cats.unshift(name);
+    cats.unshift(name);
 }
-//  removes the last cat from the cats array
-    function destructivelyRemoveLastCat(name) {
-    return cats.pop(name);
-    }
-// removes the first cat from the cats array
-    function destructivelyRemoveFirstCat(name) {
-      return  cats.shift(name);
- }
-// appends a cat to the cats array and returns a new array, leaving the cats array unchanged
-/** Still having trouble with append ad prepend, look into it! */
-    
-function appendCat(name) {
-       return [...cats,name];
-     
-}
-// prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
-    
-function prependCat(name) {
-        return [name,...cats];
- }
-// removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
-  
-function removeLastCat() {
-         return cats.slice(0,-1);    
-}
-// removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
 
-     function removeFirstCat(){
-        return cats.slice(1);
+// destructivelyRemoveLastCat()
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+//destructivelyRemoveFirstCat()
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+//appendCat(name)
+function appendCat(name){
+    return [...cats,name];
+}
+
+//prependCat(name)
+function prependCat(name){
+    return [name,...cats];
+}
+
+removeLastCat()
+function removeLastCat(){
+    return cats.slice(0,-1);
+}
+
+//removeFirstCat()
+function removeFirstCat(){
+    return cats.slice(1);
 }
